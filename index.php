8227a30dbcc5a0ffe163ac6ffda19822994760c9
dino001 <noone456gm@gmail.com>
2013-11-10 17:17:44 -0800

    Generate random arrays

--- index.php
+++ index.php
@@ -5,9 +5,19 @@
         <meta http-equiv="Cache-Control" content="no-cache">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Dual N-Back</title>
-        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="js/nback.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/main.css">        
     </head>
     <body>
-
+        <div id="wrapper">
+            <div id="header">
+                <input type="button" value="Start" />
+            </div>
+            <div id="body">
+            </div>
+            <div id="footer"> 
+            </div>
+        </div>
     </body>
 </html>
